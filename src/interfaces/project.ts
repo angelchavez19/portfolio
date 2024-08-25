45f3c43da038b16417652be536b6207ef47ff086
angelchavez19 <infoangelchavez@gmail.com>
2024-08-24 19:09:25 -0500

    page-index- Index projects

---
+++ src/interfaces/project.ts
@@ -0,0 +1,12 @@
+export interface ProjectI {
+  name: string;
+  description: string;
+  blog_link: string;
+  github_link: string;
+  demo_link: string;
+  website_link: string;
+  image: string;
+  technologies: string[];
+  date: string;
+  duration: string;
+}
